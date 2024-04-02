6303a9641fdfa92bfd64a3bee83eedb15b395462
Dagostini01 <pedro01dagostini@gmail.com>
2024-04-01 22:20:14 -0300

    consumindo stripe no front

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import ProductList from "@/components/shop/product-list";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Home</>
+    <section className="container">
+      <ProductList />
+    </section>
   );
 }
--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -22,7 +22,7 @@ export default function UserNav() {
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="/avatars/01.png" alt="@devadagostini" />
+                        <AvatarImage src="" alt="@devadagostini" />
                         <AvatarFallback>PD</AvatarFallback>
                     </Avatar>
                 </Button>
